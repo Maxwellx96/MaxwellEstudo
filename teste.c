a72bf1f9ed8c5e10f992b7606eb85dc5423d0926
Maxwellx96 <49194750+Maxwellx96@users.noreply.github.com>
2023-07-13 17:00:52 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,86 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define tamanho 51
-
-struct Produtos
-{
-    float valorProduto;
-    char nomeProduto[tamanho];
-};
-
-struct Produtos cadProduto;
+#include <locale.h>
+#include <windows.h>
 
 int op, menu = 1;
+int *pMenu = &menu;
 
-void marcaHP();
+void listToner();
 
-void main()
+int main()
 {
-    while(menu != 0)
+    while (*pMenu != 0)
     {
-        printf("--------------------------\n");
-        printf("----------[Menu]----------\n");
-        printf("--------------------------\n\n");
-        printf("[1] HP\n");
-        printf("[2] Brother\n");
-        printf("[3] Samsung\n");
-        printf("[0] Sair\n");
-        printf("Escolha o que deseja fazer: ");
+        setlocale(LC_ALL, "");
+        printf("------------------------------------\n");
+        printf("----------[Menu Principal]----------\n");
+        printf("------------------------------------\n");
+        printf("[1]HP\n");
+        printf("[2]Brother\n");
+        printf("[3]Samsung\n");
+        printf("[0]Sair\n");
+        printf("O que deseja fazer? ");
+        scanf("%d", &*pMenu);
 
-        scanf("%d", &op);
-
-        switch (op)
+        switch (*pMenu)
         {
-        case 0:
-            system("exit");
-            break;
-        case 1:
-            menu = 0;
-            marcaHP();
-        default:
-            break;
-        }
+            case 0:
+                system("pause");
+                exit(1);
+                break;
+            case 1:
+                *pMenu = 1;
+                listToner(*pMenu);
+                break;
+            case 2:
+                *pMenu = 2;
+                listToner(*pMenu);
+                break;
+            case 3:
+                *pMenu = 3;
+                listToner(*pMenu);
+                break;            
+            default:
+                break;
+        }       
     }
+    
 }
 
-void marcaHP()
-{
-    int tonerOP;
-    printf("Deseja cadastrar [1], listar [2] ou buscar [3]? \n");
-    scanf("%d", &tonerOP);
+void listToner()
+{   
+    setlocale(LC_ALL, "");
 
-    if(tonerOP == 1)
+    FILE *arquivoLeitura;
+
+    switch (*pMenu)
     {
-        printf("Digite o valor do produto: ");
-        scanf("%f", &cadProduto.valorProduto);
-        printf("Digite o nome do produto: ");
-        scanf("%[^\n]", cadProduto.valorProduto);
+        case 1:
+            arquivoLeitura = fopen("TonerHP.txt", "r");
+            break;
+        case 2:
+            arquivoLeitura = fopen("TonerBrother.txt", "r");
+            break;
+        case 3:
+            arquivoLeitura = fopen("TonerSamsung.txt", "r");
+            break;
+        
+        default:
+            break;
+    }    
 
+    if(arquivoLeitura == NULL)
+    {
+        printf("O arquivo não existe e está sendo criado");
+        if(*pMenu == 1){arquivoLeitura = fopen("TonerHP.txt", "w");}
+        if(*pMenu == 2){arquivoLeitura = fopen("TonerBrother.txt", "w");}
+        if(*pMenu == 3){arquivoLeitura = fopen("TonerSamsung.txt", "w");}                
+        printf("\nArquivo criado com sucesso!\n");
     }else
     {
-        FILE *arq;
-        char Linha[100];
-        char *result;
-
-        system("cls");
-
-        // Abre um arquivo TEXTO para LEITURA
-        arq = fopen("TonerHP.txt", "rt");
-
-        if (arq == NULL)  // Se houve erro na abertura
-        {
-            printf("Problemas na abertura do arquivo\n");
-            return;
-        }
-
-        while (!feof(arq))
-        {
-        // Lê uma linha (inclusive com o '\n')
-            result = fgets(Linha, 100, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
-            if (result)  // Se foi possível ler
-            printf("%s", Linha);
-        }
-        fclose(arq);
+        
     }
+
+
 }
