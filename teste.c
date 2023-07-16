3b8ae342465df7bf649228b83ecd79d09f362efb
Maxwellx96 <49194750+Maxwellx96@users.noreply.github.com>
2023-07-15 23:51:01 -0300

    Update teste.c
    Função para cadastro de produtos criada.
    Limpeza em algumas partes do código.
    Comentários adicionados.

--- teste.c
+++ teste.c
@@ -8,17 +8,24 @@
 
 int  menu = 1;
 int *pMenu = &menu;
+char *resultados;
+char leituraItens[tamanhoString];
+char marcaNome[countItens][tamanhoString] = {"TonerHP.txt", "TonerBrother.txt", "TonerSamsung.txt"};
+char exibirMarca[countItens][tamanhoString] = {"Toner HP", "Toner Brother", "Toner Samsung"};
+
+FILE *arquivoLeitura, *arquivoEscrita;    
 
 void listToner();
+void regToner();
 
 int main()
 {
     while (*pMenu != 0)
     {
         setlocale(LC_ALL, "");
-        printf("------------------------------------\n");
-        printf("----------[Menu Principal]----------\n");
-        printf("------------------------------------\n");
+        printf("-----------------------------------------\n");
+        printf("[Menu Principal]\n");
+        printf("-----------------------------------------\n");
         printf("[1]HP\n");
         printf("[2]Brother\n");
         printf("[3]Samsung\n");
@@ -32,15 +39,15 @@ int main()
                 exit(1);
                 break;
             case 1:
-                listToner(*pMenu);
+                listToner();
                 *pMenu = 0;
                 break;
             case 2:
-                listToner(*pMenu);
+                regToner();
                 *pMenu = 0;
                 break;
             case 3:
-                listToner(*pMenu);
+                listToner();
                 *pMenu = 0;
                 break;          
             default:
@@ -49,36 +56,84 @@ int main()
     }    
 }
 
-void listToner()
+//Função para cadastrar produto
+void regToner()
 {   
+    //Define o idioma padrão do sistema.
+    setlocale(LC_ALL, "");
+    
+    int continueCad;
+    char produtoEspec[2][20];
+    
+    // Abrir o arquivo da respectiva marca escolhida para escrita.
+    arquivoEscrita = fopen(marcaNome[*pMenu-1], "a");
+
+     //Se o arquivo não existir, ele será criado.
+    if(arquivoEscrita == NULL)
+    {
+        arquivoEscrita = fopen(marcaNome[*pMenu-1], "w");
+        fclose(arquivoEscrita);
+        main();        
+    }
+
+     //Caso o arquivo já exista, opções para cadastro serão exibidas na tela.
+    else
+    {
+        system("cls");
+        printf("-----------------------------------------\n");
+        printf("Cadastrar Toner\n");
+        printf("-----------------------------------------\n");
+
+        printf("Digite o nome do produto: ");
+        fflush(stdin);
+        scanf("%[^\n]", &produtoEspec[0]);
+
+        printf("Digite o valor do produto: ");
+        fflush(stdin);
+        scanf("%[^\n]", &produtoEspec[1]);
+
+        //Concatena o nome do produto e valor já incluindo o R$ e quebra de linha ao final.
+        strcat(produtoEspec[0], " - R$");
+        strcat(produtoEspec[0], produtoEspec[1]);
+        strcat(produtoEspec[0], "\n");
+
+        //Insere os valores no arquivo TXT da marca escolhida e fecha o arquivo.
+        fputs(produtoEspec[0], arquivoEscrita);
+        fclose(arquivoEscrita);
+
+        printf("Deseja continuar cadastrando algum produto? [1] Sim | [0]Não: ");
+        scanf("%d", &continueCad);
+
+        (continueCad == 1) ? regToner() : main();
+    }
+}
+
+//Listar os preços cadastrados para cada marca.
+void listToner()
+{      
+    //Define o idioma padrão do sistema.
     setlocale(LC_ALL, "");
 
-    char *resultados;
-    char leituraItens[tamanhoString];
-    char marcaNome[countItens][tamanhoString] = {"TonerHP.txt", "TonerBrother.txt", "TonerSamsung.txt"};
+    // Abrir o arquivo da respectiva marca escolhida para leitura apenas.
+    arquivoLeitura = fopen (marcaNome[*pMenu-1], "r"); 
 
-    FILE *arquivoLeitura, *arquivoEscrita;    
-    arquivoLeitura = fopen (marcaNome[*pMenu-1], "r");
-
+    //Se o arquivo não existir, ele será criado.
     if(arquivoLeitura == NULL)
     {
-        printf("O arquivo não existe e está sendo criado...\n");
-        printf("Arquivo criado com sucesso!\n");
-        printf("Você será redirecionado para o Menu Principal\n");
         arquivoEscrita = fopen(marcaNome[*pMenu-1], "w");
+        fclose(arquivoEscrita);
+        main();        
+    }
+    
+    //Caso o arquivo já exista, a lista de todos os produtos cadastrados serão exibidos na tela.
+    else
+    {
+        system("cls");
+        printf("-----------------------------------------\n");
+        printf("[%s]\n", exibirMarca[*pMenu-1]);
+        printf("-----------------------------------------\n");
 
-        Sleep(1500);
-        system("cls");
-        main();
-     }
-     
-     else
-     {
-        system("cls");
-        printf("------------------------------------\n");
-        printf("-------------[Toner HP]-------------\n");
-        printf("------------------------------------\n");
-        
+        //Verifica e continua imprimindo os valores na tela até o arquivo chegar ao fim.
         while(!feof(arquivoLeitura))
         {
             resultados = fgets(leituraItens, tamanhoString, arquivoLeitura);
@@ -87,9 +142,10 @@ void listToner()
                 printf("%s", leituraItens);
             }
         }
-        
-        fclose(arquivoLeitura);
+
         system("pause");
+        system("cls");
+        fclose(arquivoLeitura);
         main();
-     }
+    }
 }
