4969400c9c6f7ed0c9c88b9876ab85103ebbcb2a
Maxwell Lima <49194750+Maxwellx96@users.noreply.github.com>
2023-09-03 21:51:00 -0300

    Create Estudo_Capitulo3.py
    Estruturas Condicionais (if, elif, else)

---
+++ Estudo_Capitulo3.py
@@ -0,0 +1,13 @@
+#Estruturas Condicionais
+
+# 1) Em estatística, uma mediana é um valor que divide uma amostra ao meio, com os elementos menores que a mediana à esquerda desta; e os maiores, à direita. Faça um programa que solicita três 
+# números inteiros ao usuário e imprime a média aritmética simples e a mediana dos três. A mediana, em muitos casos, é uma medida de tendência central mais adequada que a média, pois é menos 
+# influenciada por valores extremos.
+
+# 2) Crie um programa que solicitará ao usuário as medidas de três lados de um triângulo e informará como saída se os valores formam um triângulo retângulo. Os lados devem ser fornecidos em 
+# qualquer ordem (ou seja, não “vale” escrever “digite o tamanho da hipotenusa”, “digite o tamanho do cateto 1” etc.). Dica: use o Teorema de Pitágoras (“O quadrado da hipotenusa é igual à 
+# soma dos quadrados dos catetos”).
+
+################################################################# 1 #################################################################
+
+################################################################# 2 #################################################################
